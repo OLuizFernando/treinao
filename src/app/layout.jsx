69ffc853984408a19715dc0789ef1518fd33f9a6
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-02-04 21:03:06 -0300

    change `layout.jsx` responsiveness and add a `redirect to login` conditional to `page.jsx`

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,10 +4,12 @@ import Navbar from "../components/navbar";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="antialiased bg-slate-900">
-        <Navbar></Navbar>
-        {children}
+    <html lang="pt-br" className="bg-slate-900">
+      <body className="antialiased">
+        <div className="flex flex-col h-screen">
+          <Navbar></Navbar>
+          {children}
+        </div>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return (<></>);
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  return <h1 className="text-violet-50 text-5xl">Bem vindo! Você está logado.</h1>;
 }
